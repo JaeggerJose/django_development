c75ebcb4c15c59a9025d1e217f1bec622266b297
lms025187 <lms025187@gmail.com>
2022-07-15 00:02:50 -0700

    add html

--- mblog/app/views.py
+++ mblog/app/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
@@ -6,10 +8,5 @@ from .models import Post
 
 def homepage(request):
     posts = Post.objects.all()
-    post_list = list()
-    for cout, post in enumerate(posts):
-        post_list.append("No.{}: ".format(str(cout))+str(post.title)+"<br>")
-        post_list.append("<small>"+str(post.slug.encode('utf-8'))\
-+"</small><br>"+str(post.body.encode('utf-8')+"<br><br>"))
-        post_list.append("Post date is on {}".format(str(post.pub_date))+"<br><br><br>")
-    return HttpResponse(post_list)
+    now = datetime.now()
+    return render(request , 'index.html', locals())
