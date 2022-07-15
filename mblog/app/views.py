315475da4b7f090bc3309671ad47341db5093431
lms025187 <lms025187@gmail.com>
2022-07-14 23:23:23 -0700

    computer failed, thus need to commit in order to commit the file

--- mblog/app/views.py
+++ mblog/app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
+
+def homepage(request):
+    posts = Post.objects.all()
+    post_list = list()
+    for cout, post in enumerate(posts):
+        post_list.append("No.{}: ".format(str(cout))+str(post.title)+"<br>")
+        post_list.append("<small>"+str(post.slug.encode('utf-8'))\
++"</small><br>"+str(post.body.encode('utf-8')+"<br><br>"))
+        post_list.append("Post date is on {}".format(str(post.pub_date))+"<br><br><br>")
+    return HttpResponse(post_list)
