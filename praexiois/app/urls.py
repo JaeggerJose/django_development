4ba854faf81a93eb9e776ae9098b2c8d2a9552e2
lms025187 <lms025187@gmail.com>
2022-07-25 15:57:17 +0800

    25.07.2022 afternoon

--- praexiois/app/urls.py
+++ praexiois/app/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'app'
 urlpatterns = [
     path('', views.IndexView, name='index'),
-    path('/create/', views.create, name='create'),
-    path('/create_active/', views.create_active, name='create_active'),
+    path('create/', views.create, name='create'),
+    path('create/active/', views.create_active, name='create_active'),
 ]
--- praexiois/app/views.py
+++ praexiois/app/views.py
@@ -1,8 +1,7 @@
-from time import sleep
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-import time, os
+import time, os, random
 
 
 def IndexView(request):
@@ -12,17 +11,19 @@ def create(request):
     return render(request, 'app/create.html')
 
 def create_active(request):
+    name  = random.randint(100,999)
     f = open('/home/minghsuan/Desktop/Job/job.sh','w+')
     f.write("#!/bin/bash\n")
     f.write("#SBATCH --job-name=$jobName\n")
-    f.write("#SBATCH --ntasks=1\n")
-    f.write("#SBATCH --cpus-per-task=1\n")
+    f.write("#SBATCH --ntasks={}\n".format(8))
+    f.write("#SBATCH --cpus-per-task=\n")
     f.write("#SBATCH --mem=1gb\n")
-    f.write("#SBATCH --output=/home/minghsuan/Desktop/Job/output.log\n")
+    f.write("#SBATCH --output=/tmp/output.log\n")
     f.write("#SBATCH --partition=COMPUTE1Q\n")
     f.write("#SBATCH --account=root\n" )
     f.write("echo '5'\n")
     f.close()
-    time.sleep(2) 
-    os.system('sbatch /home/minghsuan/Desktop/Job/job.sh')
-    return render(request, 'app/index.html')
+    time.sleep(3)
+    os.system('mv /home/minghsuan/Desktop/Job/job.sh /tmp')
+    os.system('sbtach job.sh')
+    return HttpResponseRedirect(reverse('app:index')) #The reverse var. is names of path from urls.py
